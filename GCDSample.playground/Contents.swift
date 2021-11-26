de8a301b44c7683ad1c0125ead5d57b45149897a
vudat81299 <vudat81299@gmail.com>
2021-11-26 17:40:00 +0700

    add example

--- GCDSample.playground/Contents.swift
+++ GCDSample.playground/Contents.swift
@@ -97,6 +97,35 @@ for i in 100...200 {
 
 
 
+let group = DispatchGroup()
+let queue = DispatchQueue.global(qos: .userInitiated)
+
+queue.async(group: group) {
+    print("Start job 1")
+    Thread.sleep(until: Date().addingTimeInterval(10))
+    print("End job 1")
+}
+
+queue.async(group: group) {
+    print("Start job 2")
+    Thread.sleep(until: Date().addingTimeInterval(2))
+    print("End job 2")
+}
+
+if group.wait(timeout: .now() + 5) == .timedOut {
+    print("I got tired of waiting")
+} else {
+    print("All the jobs have completed")
+}
+
+/*
+ Start job 1
+ Start job 2
+ End job 2
+ I got tired of waiting
+ End job 1
+ */
+
 
 
 
