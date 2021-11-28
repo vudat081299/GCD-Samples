2323ef824753841a2f688690d579a1225b896a88
vudat81299 <vudat81299@gmail.com>
2021-11-28 18:32:53 +0700

    add example: priority inversion, add concurrency book

--- GCDSample.playground/Contents.swift
+++ GCDSample.playground/Contents.swift
@@ -130,5 +130,113 @@ print("Passing group wait")
 
 
 
+// MARK: - Group, Semaphores
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+let gsGroup = DispatchGroup()
+let gsQueue = DispatchQueue.global(qos: .userInteractive)
+let semaphore = DispatchSemaphore(value: 2)
+
+let base = "https://wolverine.raywenderlich.com/books/con/image-from-rawpixel-id-"
+let ids = [466881, 466910, 466925, 466931, 466978, 467028, 467032, 467042, 467052]
+
+var images: [UIImage] = []
+
+for id in ids {
+    guard let url = URL(string: "\(base)\(id)-jpeg.jpg") else { continue }
+    
+    semaphore.wait()
+    gsGroup.enter()
+    
+    let task = URLSession.shared.dataTask(with: url) { data, _, error in
+        defer {
+            gsGroup.leave()
+            semaphore.signal()
+        }
+        
+        if error == nil,
+           let data = data,
+           let image = UIImage(data: data) {
+            images.append(image)
+        }
+    }
+    
+    task.resume()
+}
+
+gsGroup.notify(queue: gsQueue) {
+    images[0]
+    
+    //: Make sure to tell the playground you're done so it stops.
+    PlaygroundPage.current.finishExecution()
+}
+
+
+
+// MARK: - Priority inversion
+// Appear when use semaphore
+let high = DispatchQueue.global(qos: .userInteractive)
+let medium = DispatchQueue.global(qos: .userInitiated)
+let low = DispatchQueue.global(qos: .background)
+let piSemaphore = DispatchSemaphore(value: 1)
+high.async {
+    piSemaphore.wait()
+    print("high")
+    Thread.sleep(forTimeInterval: 1)
+    defer {
+        piSemaphore.signal()
+        print("High priority task is now running")
+    }
+}
+
+for i in 1...10 {
+    medium.async {
+        let waitTime = Double(exactly: arc4random_uniform(7))!
+        print("Running medium task \(i)")
+        Thread.sleep(forTimeInterval: waitTime)
+    }
+}
+
+low.async {
+    piSemaphore.wait()
+    print("low")
+    defer {
+        piSemaphore.signal()
+        print("Running long, lowest priority task")
+    }
+//    Thread.sleep(forTimeInterval: 5)
+}
+
+//high.async {
+//    Thread.sleep(forTimeInterval: 1)
+//    piSemaphore.wait()
+//    print("high")
+//    defer {
+//        piSemaphore.signal()
+//        print("High priority task is now running")
+//    }
+//}
+//
+//for i in 1...10 {
+//    medium.async {
+//        let waitTime = Double(exactly: arc4random_uniform(7))!
+//        print("Running medium task \(i)")
+//        Thread.sleep(forTimeInterval: waitTime)
+//    }
+//}
+//
+//low.async {
+//    piSemaphore.wait()
+//    print("low")
+//    defer {
+//        piSemaphore.signal()
+//        print("Running long, lowest priority task")
+//    }
+//    Thread.sleep(forTimeInterval: 5)
+//}
+
+
+
+
 
 PlaygroundPage.current.finishExecution()
